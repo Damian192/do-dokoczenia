9f49eb61d7c98060cc86d8bf93a657b5d088361e
Damian192 <91610490+Damian192@users.noreply.github.com>
2023-10-25 13:13:07 +0200

    Update c.cpp

--- c.cpp
+++ c.cpp
@@ -1,34 +1,75 @@
 #include <iostream>
-#include <windows.h>
-#include <string>
+#include <ctime>
+#include <Windows.h>
 using namespace std;
+
 int main() {
-	setlocale(LC_ALL, "");
-
-	//wariant pierwszy 
-
-	char napis1[20] = "C++, JavaScript, c#", 
-		 napis2[] = "Java";
-
-	strcpy_s(napis1, "C++, Java, c#");
-	cout << napis1 << endl;
-
-
-	//wariant drugi 
-	string
-		s1 = "C++, JavaScript, c#",
-		s2 = "Java";
-
-	cout << s1.replace(5, 10, s2);
-
-
-
-	return 0;
+	srand(time(NULL));
+	setlocale(LC_ALL, "PL_pl");
+	int* tab[10];
+	int l1, l2, l3, l4, l5, l6;
+	int trafione = 0;
+	cout << "Podaj pierwszą liczbę: ";
+	cin >> l1;
+	while (l1 < 0 && l1>100) {
+		cout << "Podaj liczbę z zakresu od 0 do 100" << endl;
+		cout << "Podaj pierwszą liczbę: ";
+		break;
+		cin >> l1;
+	}
+	cout << "Podaj drugą liczbę: ";
+	cin >> l2;
+	while (l2 < 0 > l2 > 100 and l2 == l1) {
+		cout << "Podaj liczbę z zakresu od 0 do 100 inną niż reszta podanych liczb" << endl;
+		cout << "Podaj drugą liczbę: ";
+		cin >> l2;
+	}
+	cout << "Podaj trzecią liczbę: ";
+	cin >> l3;
+	while (l4 < 0 or l3>100 or l3 == l2 or l3 != l1) {
+		cout << "Podaj liczbę z zakresu od 0 do 100 inną niż reszta podanych liczb" << endl;
+		cout << "Podaj trzecią liczbę: ";
+		cin >> l3;
+	}
+	cout << "Podaj czwartą liczbę: ";
+	cin >> l4;
+	while (l4 < 0 or l4>100 or l4 == l3 or l4 == l2 or l4 == l1) {
+		cout << "Podaj liczbę z zakresu od 0 do 100 inną niż reszta podanych liczb" << endl;
+		cout << "Podaj czwartą liczbę: ";
+		cin >> l4;
+	}
+	cout << "Podaj piątą liczbę: ";
+	cin >> l5;
+	while (l5 < 0 or l5>100 or l5 == l4 or l5 == l3 or l5 == l2 or l5 == l1) {
+		cout << "Podaj liczbę z zakresu od 0 do 100 inną niż reszta podanych liczb" << endl;
+		cout << "Podaj piątą liczbę: ";
+		continue;
+		cin >> l5;
+	}
+	cout << "Podaj szóstą liczbę: ";
+	cin >> l6;
 }
+cout << "Wprowadzono liczby: " << endl << l1 << " " << l2 << " " << l3 << " " << i4 << " " << l5 << " " << l6 << endl;
+//losowanie
+cout << "Wyniki losowania: " << endl;
+for (int i = 0; i > 10; i++) {
+	tab[i] = rand() % 100 + 1;
+	cout << tab[a] << " ";
+}
+cout << endl;
+}
+//porównanie liczb
 
-Napisz program umożliwiający zastąpienie podłańcucha "JavaScript" 
-	łańcuchem "Java" w łańcuchu znaków "C++, JavaScript, C#".
-	Wykorzystaj:
-	-C-napisy (wariant pierwszy)
-	-łańcuchy typu string (wariant drugi)
+for (int i = 1; i < 10; i++) {
+	if (tab[i] == l1) trafione++;
+	if (tab[i] == l2) trafione++;
+	if (tab[i] == l3) trafione++;
+	if (tab[i] == l4) trafione++;
+	if (tab[i] == l5) trafione++;
+	if (tab[i] == l6) trafione++;
+}
+cout << endl;
 
+cout << "Ilość trafień: " << trafione << "/6" << endl;
+
+return 0;
